92b59f22ff4e06e3c229c7bfd3a216305eb4e21a
marvinandrew.rosales <marvinandrew.rosales@gcash.com>
2025-07-11 17:14:02 +0800

    feat: animal instantiate in each enclosure

--- src/main/java/org/example/Buildings/Enclosures/FelineEnclosure.java
+++ src/main/java/org/example/Buildings/Enclosures/FelineEnclosure.java
@@ -1,8 +1,11 @@
 package org.example.Buildings.Enclosures;
 
+import org.example.entity.Lion;
+
 public class FelineEnclosure extends Enclosure{
 
     public FelineEnclosure(String staff) {
         super(staff);
+        animals.addLast(new Lion("Mufasa", true));
     }
 }
