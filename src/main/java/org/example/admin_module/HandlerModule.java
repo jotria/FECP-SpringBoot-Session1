93182b2bf0ca9fcec7f52ffc44d6a9ff06d3d7d4
jotria <joana.tria@gcash.com>
2025-07-11 13:39:20 +0800

    feat: added animal duty menu

--- src/main/java/org/example/admin_module/HandlerModule.java
+++ src/main/java/org/example/admin_module/HandlerModule.java
@@ -37,10 +37,28 @@ public class HandlerModule {
         }
     }
 
-    public void animalDutyMenu(){
-        if(animalHandlerCheck()){
+    public void animalDutyMenu() {
+        if (animalHandlerCheck()) {
             System.out.println("\n--- Animal Duty Menu ---");
 
+            // Match handler to the appropriate enclosure
+            if (handlerName.equals(adminModule.pachydermEnclosure.getStaff())) {
+                System.out.println("Pachyderm Enclosure Animals:");
+                for (String animal : adminModule.pachydermEnclosure.getAnimals()) {
+                    System.out.println("- " + animal);
+                }
+            } else if (handlerName.equals(adminModule.felineEnclosure.getStaff())) {
+                System.out.println("Feline Enclosure Animals:");
+                for (String animal : adminModule.felineEnclosure.getAnimals()) {
+                    System.out.println("- " + animal);
+                }
+            } else if (handlerName.equals(adminModule.birdEnclosure.getStaff())) {
+                System.out.println("Bird Enclosure Animals:");
+                for (String animal : adminModule.birdEnclosure.getAnimals()) {
+                    System.out.println("- " + animal);
+                }
+            }
         }
     }
+
 }
